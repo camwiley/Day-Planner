563d00d8652f5eb61dc5a6a860e4bc041447e0c2
Cam Wiley <insuredbycam@gmail.com>
2021-07-27 20:32:43 -0400

    Created function to chagne text area to past present or future

--- assets/script.js
+++ assets/script.js
@@ -32,3 +32,22 @@ saveBtn.on("click", function () {
 
     localStorage.setItem(time, schedule);
 });
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
